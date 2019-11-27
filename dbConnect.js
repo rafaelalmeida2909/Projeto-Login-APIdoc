8eb6e6042f53ae9894fdfc372e31d574709f1c11
Francisco Ossian <42554801+FranciscoOssianFOLN@users.noreply.github.com>
2019-11-27 12:53:17 -0300

    Correção do type da url
    Obj -> string

--- dbConnect.js
+++ dbConnect.js
@@ -6,7 +6,7 @@
  * @apiDescription Método para conexão com o banco de dados. A API usará o mongodb como base de dados no-SQL. Cada rota será implementada usando a framework expressjs, e dentro de cada rota haverá uma chamada para o objeto da base de dados. Com isso, em cada rota haverá a possibilidade de solicitar ou enviar novos dados para a base.
  *
  * @apiParam {Object} mongoClient Objeto de conexão do pacote mongodb.
- * @apiParam {Object} url Objeto contendo url de conexão com a base de dados.
+ * @apiParam {String} url Objeto contendo url de conexão com a base de dados.
  * @apiParam {Object} err Objeto de erro gerado quando a conexão não funciona.
  * @apiParam {Object} client Objeto do cliente, usado para acesso a base de dados desejada.
  * @apiParam {Object} db Variável para acesso as coleções da base de dados.
